20fb7fbd74b75afceb7a20f42b4bf0bb5708a826
Jon Long <jonbrantleylong@gmail.com>
2020-08-23 17:32:33 -0400

    make the timing better lol

--- bot.py
+++ bot.py
@@ -18,19 +18,23 @@ async def check_factorio_stats():
   channel = client.get_channel(747152188631154748)
 
   while not client.is_closed():
-    playing = []
-    for member in client.guilds[0].members:
-      if member.activity and member.activity.name == "Factorio":
-        time_played = (datetime.datetime.now(tz=timezone.utc) - member.activity.created_at.replace(tzinfo = timezone.utc))
-        hours = time_played.seconds // 3600
-        minutes = (time_played.seconds // 60) % 60
+    await channel.send(get_play_time())
+    await asyncio.sleep(3600)
 
-        playing.append(f'{member.nick}: {hours} hours and {minutes} minutes')
-    str = 'Current fiends:'
-    for ch in playing:
-      str = str + '\n' + '   ' + ch
-    await channel.send(str)
-    await asyncio.sleep(60)    
+def get_play_time():
+  playing = []
+  for member in client.guilds[0].members:
+    if member.activity and member.activity.name == "Factorio":
+      time_played = (datetime.datetime.now(tz=timezone.utc) - member.activity.created_at.replace(tzinfo = timezone.utc))
+      hours = time_played.seconds // 3600
+      minutes = (time_played.seconds // 60) % 60
+
+      playing.append(f'{member.nick}: {hours} hours and {minutes} minutes')
+  str = 'Current fiends:'
+  for ch in playing:
+    str = str + '\n' + '   ' + ch
+  
+  return str
 
 @client.event
 async def on_ready():
@@ -40,6 +44,14 @@ async def on_ready():
   print(channel.name)
   print(f'{client.user} has connected to Discord!')
   await channel.send('Connected successfully!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$cracktorio'):
+        await message.channel.send(get_play_time())
 
 client.loop.create_task(check_factorio_stats())
 client.run(TOKEN)
